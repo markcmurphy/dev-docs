1f36d616a06d2cbc9f0d8ad5b232a51f261228c9
Mark Murphy <mark@murphymark.me>
2021-02-11 12:27:06 -0600

    Update mdlint.sh

--- mdlint.sh
+++ mdlint.sh
@@ -2,8 +2,8 @@
 files=`(git fetch origin master:master) && (git diff --name-only master)`
 for x in $files;
 do
-if [ ${x: -3} == ".md" ]
-then
-node_modules/.bin/markdownlint $x
-fi
+	if [ ${x: -3} == ".md" ]
+	then
+		node_modules/.bin/markdownlint $x
+	fi
 done
